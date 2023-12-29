c9e84553c900cf12a7f01832be0ee37cde7913c6
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2023-12-29 17:02:48 +0000

    added an if statement to check if there is an existing score stored locally and if so parse it and add the initials and score to the highscores list

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -4,3 +4,14 @@ clearButton.addEventListener('click', function (event) {
   event.preventDefault();
   localStorage.clear();
 })
+
+var highScores = document.querySelector('#highscores');
+var li = document.createElement('li');
+var existingStoredScore = localStorage.getItem('highScore');
+
+if (existingStoredScore) {
+
+  existingStoredScore = JSON.parse(existingStoredScore);
+  li.innerText = `${existingStoredScore.initials} - ${existingStoredScore.score}`
+  highScores.append(li);
+}
