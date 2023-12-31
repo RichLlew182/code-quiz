c9cabc050bdeaa5d4f5766235230de1e90b61419
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2023-12-31 11:42:24 +0000

    rearranged the questions into an array of objects

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -22,3 +22,35 @@ var questionFive = {
   question: 'What is the main purpose of the HTML "div" element?',
   answers: ['Define a hyperlink', 'Create a division or a section', 'Add an image', 'Style text']
 }
+
+
+/* Refactor */
+
+var q & s = [
+
+  {
+    questionOne: 'What does HTML stand for?',
+    questionOneAnswers: ['HyperText Markup Language', 'HyperTransfer Text Language', 'HyperTech Machine Language', 'HyperText and Links Markup']
+  },
+
+  {
+    questionTwo: 'Which programming language is known as the "language of the web"?',
+    questionTwoAnswers: ['Python', 'C++', 'JavaScript', 'Java']
+  },
+
+  {
+    questionThree: 'What does CSS stand for?',
+    questionThreeAnswers: ['Computer Style Sheets', 'Creative Style Sheets', 'Cascading Style Sheets', 'Colorful Style Sheets']
+  },
+
+  {
+    questionFour: 'Which of the following is a version control system?',
+    questionFourAnswers: ['Photoshop', 'Git', 'Excel', 'Word']
+  },
+
+  {
+    questionFive: 'What is the main purpose of the HTML "div" element?',
+    questionFiveAnswers: ['Define a hyperlink', 'Create a division or a section', 'Add an image', 'Style text']
+  }
+
+]
