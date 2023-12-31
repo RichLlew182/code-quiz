2c3a1fdbc4de7285a4d36c737b80438fcac57db9
Richard Llewellyn <richard.lee.llewellyn@gmail.com>
2023-12-31 11:47:14 +0000

    changed the names of each question object in the array to be more general to make them easier to call

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -26,31 +26,31 @@ var questionFive = {
 
 /* Refactor */
 
-var q & s = [
+var qAndAs = [
 
   {
-    questionOne: 'What does HTML stand for?',
-    questionOneAnswers: ['HyperText Markup Language', 'HyperTransfer Text Language', 'HyperTech Machine Language', 'HyperText and Links Markup']
+    question: 'What does HTML stand for?',
+    answers: ['HyperText Markup Language', 'HyperTransfer Text Language', 'HyperTech Machine Language', 'HyperText and Links Markup']
   },
 
   {
-    questionTwo: 'Which programming language is known as the "language of the web"?',
-    questionTwoAnswers: ['Python', 'C++', 'JavaScript', 'Java']
+    question: 'Which programming language is known as the "language of the web"?',
+    answers: ['Python', 'C++', 'JavaScript', 'Java']
   },
 
   {
-    questionThree: 'What does CSS stand for?',
-    questionThreeAnswers: ['Computer Style Sheets', 'Creative Style Sheets', 'Cascading Style Sheets', 'Colorful Style Sheets']
+    question: 'What does CSS stand for?',
+    answers: ['Computer Style Sheets', 'Creative Style Sheets', 'Cascading Style Sheets', 'Colorful Style Sheets']
   },
 
   {
-    questionFour: 'Which of the following is a version control system?',
-    questionFourAnswers: ['Photoshop', 'Git', 'Excel', 'Word']
+    question: 'Which of the following is a version control system?',
+    answers: ['Photoshop', 'Git', 'Excel', 'Word']
   },
 
   {
-    questionFive: 'What is the main purpose of the HTML "div" element?',
-    questionFiveAnswers: ['Define a hyperlink', 'Create a division or a section', 'Add an image', 'Style text']
+    question: 'What is the main purpose of the HTML "div" element?',
+    answers: ['Define a hyperlink', 'Create a division or a section', 'Add an image', 'Style text']
   }
 
 ]
